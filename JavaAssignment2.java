76f888b6d0a092346d9557a6428b0a4ab85b8d53
Ayushi Srivastava <ayushirensa@gmail.com>
2023-09-27 21:29:07 +0530

    New file added

---
+++ JavaAssignment2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long length = 0;
+        int i = 0;
+
+        while (length < k) {
+            if (Character.isDigit(s.charAt(i))) {
+                length *= s.charAt(i) - '0';
+            } else {
+                length++;
+            }
+            i++;
+        }
+
+        for (int j = i - 1; j >= 0; j--) {
+            if (Character.isDigit(s.charAt(j))) {
+                length /= s.charAt(j) - '0';
+                k %= length;
+            } else {
+                if (k == 0 || k == length) {
+                    return Character.toString(s.charAt(j));
+                }
+                length--;
+            }
+        }
+
+        return "";
+    }
+}
+
+public class JavaAssignment2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter k:");
+        int k=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter s:");
+        String s=sc.nextLine();
+        Solution obj=new Solution();
+        System.out.println(obj.decodeAtIndex(s, k));
+        sc.close();
+    }
+}
